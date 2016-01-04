22ef1d57779e4ab56fc61ddd53f621509b330b71
Slava Petrov <slavik--petrov@yandex.ru>
2016-01-04 15:33:34 +0300

    Show post details

--- app.rb
+++ app.rb
@@ -53,6 +53,9 @@ end
 get '/details/:post_id' do
 	post_id = params[:post_id]	
 
-	erb "показываем результаты для поста #{post_id}"
+	results = @db.execute 'select * from posts where id = ?', [post_id]
+	@row = results[0]
+
+	erb :details
 
 end
