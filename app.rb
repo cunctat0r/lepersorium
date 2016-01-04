77fe3f9cdad9c2bd7e74da8c58f6f20ec503f0ac
Slava Petrov <slavik--petrov@yandex.ru>
2016-01-04 16:12:53 +0300

    Add table Comments

--- app.rb
+++ app.rb
@@ -21,6 +21,13 @@ configure do
 		created_date DATE, 
 		content TEXT
 	)'
+	@db.execute 'create table if not exists Comments 
+	(
+		id INTEGER PRIMARY KEY AUTOINCREMENT, 
+		created_date DATE, 
+		content TEXT,
+		post_id INTEGER
+	)'
 end
 
 
